df4327196030ea00c16e985c4f894ad16243c6cd
Samrat Kundu <samrat15kundu@gmail.com>
2023-05-04 20:29:47 +0530

    javascript codes

---
+++ Practice.js
@@ -0,0 +1,28 @@
+// ****Javascript Fundamental****
+// let a;
+// console.log(typeof a);
+// let str='hello';
+// console.log(typeof str);
+// let num=1797;
+// console.log(typeof num);
+// let bol=true;
+// console.log(typeof bol);
+// let obj=
+// { 
+//     name:'Samrat',
+//     age:29,
+//     sal:40000
+// };
+// console.log(typeof obj);
+// let arr=[2,4,5];
+// console.log(typeof arr);
+// let bignum=99999999n;
+// console.log(typeof bignum);
+
+// ****Nullish Coalescing Operator***
+
+// const name = null ?? 'John';
+// console.log(name);
+// const name1 =undefined ?? 'John';
+// console.log(name1);
+
